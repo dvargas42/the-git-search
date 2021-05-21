578f37085fb72a9b4744532a3bbee3efd7f8dc3f
dvargas42 <dvargas.eng@gmail.com>
2021-05-21 17:04:41 -0300

    file creation

---
+++ src/pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, {Html, Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html >
+      <Head>
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link 
+        href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" 
+        rel="stylesheet" 
+      />
+
+      <link rel="shortcut icon" href="/favicon.svg" type="image/svg" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+      </Html>
+    )
+  }
+}
