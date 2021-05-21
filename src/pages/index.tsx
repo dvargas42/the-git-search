35110be2b65ce18cca26067597e2d29fe0945779
dvargas42 <dvargas.eng@gmail.com>
2021-05-21 19:02:51 -0300

    Footer component importation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import { Footer } from '../components/Footer'
 import { Header } from '../components/Header'
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   )
 }
