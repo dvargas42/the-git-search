b6fb9c32be49360c72e41b41117f049d3643f3b6
dvargas42 <dvargas.eng@gmail.com>
2021-05-21 19:01:26 -0300

    file creation

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import styles from './footer.module.scss'
+
+export function Footer() {
+  return (
+    <div className={styles.footerContainer}>
+      <h1>Projetado por: Daniel Vargas - 23/05/2021</h1>
+    </div>
+  )
+}
