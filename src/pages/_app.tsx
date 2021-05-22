6b70cf931e27d2b47f040a4f1e61c4da0af157c7
dvargas42 <dvargas.eng@gmail.com>
2021-05-22 14:49:33 -0300

    Header and Footer components importation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import { AppProps } from 'next/app'
+
+import { Footer } from '../components/Footer'
+import { Header } from '../components/Header'
+
 import '../styles/global.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  )
 }
 
 export default MyApp
