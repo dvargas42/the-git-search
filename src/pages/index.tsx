f74b3f2ea18314acdbe19900f75bc8264a9dd020
dvargas42 <dvargas.eng@gmail.com>
2021-05-21 23:49:29 -0300

    styles import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
+import Head from 'next/head'
 import { Footer } from '../components/Footer'
 import { Header } from '../components/Header'
+
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
