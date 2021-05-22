481b458bf9ac7a859c3109fb5dc603a16e642a12
dvargas42 <dvargas.eng@gmail.com>
2021-05-21 23:58:57 -0300

    placeholder property inclusion

--- src/components/Header/SearchBox.tsx
+++ src/components/Header/SearchBox.tsx
@@ -3,7 +3,7 @@ import styles from './searchBox.module.scss'
 export function SearchBox() {
   return (
     <label className={styles.searchBoxContainer}>
-      <input type="text" />
+      <input type="text" placeholder="Pesquisar" />
       <button>
         <img src="/images/Magnifier.svg" alt="Lupa" />
       </button>
