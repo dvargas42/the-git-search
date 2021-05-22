84f873900d27e70a56877c2215a05cee0eb84ac2
dvargas42 <dvargas.eng@gmail.com>
2021-05-21 23:51:13 -0300

    body implementation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,12 @@ export default function Home() {
   return (
     <>
       <Header />
+      <main className={styles.homeContainer}>
+        <Head>
+          <title>Home | The Git Search</title>
+        </Head>
+          <img className={styles.homeContent} src="/images/LogotypeShadow.svg" alt="hero" />
+      </main>
       <Footer />
     </>
   )
