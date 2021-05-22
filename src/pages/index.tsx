cc56d85267d83174402552df264a8f245ba6cc31
dvargas42 <dvargas.eng@gmail.com>
2021-05-22 14:51:06 -0300

    Header and Footer components removed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,16 @@
 import Head from 'next/head'
-import { Footer } from '../components/Footer'
-import { Header } from '../components/Header'
 
 import styles from './home.module.scss'
 
 export default function Home() {
   return (
     <>
-      <Header />
       <main className={styles.homeContainer}>
         <Head>
           <title>Home | The Git Search</title>
         </Head>
           <img className={styles.homeContent} src="/images/LogotypeShadow.svg" alt="hero" />
       </main>
-      <Footer />
     </>
   )
 }
