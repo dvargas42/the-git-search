4b665f8d03248ef33d0f2c8a6ded9378d90914f4
dvargas42 <dvargas.eng@gmail.com>
2021-05-23 01:57:54 -0300

    user modal implementation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,19 @@
 import { AppProps } from 'next/app'
+import Modal from 'react-modal';
 
 import { Footer } from '../components/Footer'
 import { Header } from '../components/Header'
+import { UserModal } from '../components/UserModal';
 
 import '../styles/global.scss'
+
+Modal.setAppElement('#__next')
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Header />
+      <UserModal isOpen={false} onRequestClose={() => false}/>
       <Component {...pageProps} />
       <Footer />
     </>
