ecf8d2cb1ad7effab47434fcaff40edb037c9f18
dvargas42 <dvargas.eng@gmail.com>
2021-05-23 00:54:07 -0300

    useSearchData hook/context implementation

--- src/components/Header/SearchBox.tsx
+++ src/components/Header/SearchBox.tsx
@@ -1,12 +1,34 @@
+import { useState } from 'react'
+import Link from 'next/link'
+
 import styles from './searchBox.module.scss'
+import { useSearchData } from '../../hooks/useSearchData'
 
 export function SearchBox() {
+  const { handleSearch } = useSearchData()
+  const [search, setSearch] = useState<string>()
+
+  function handleSearchInput() {
+    handleSearch(search)
+    setSearch('')
+  }
+
   return (
     <label className={styles.searchBoxContainer}>
-      <input type="text" placeholder="Pesquisar" />
-      <button>
-        <img src="/images/Magnifier.svg" alt="Lupa" />
-      </button>
+      <input
+        type="text"
+        placeholder="Pesquisar"
+        value={search}
+        onChange={event => setSearch(event.target.value)}
+      />
+      <Link href={`/results/${search}`}>
+        <button
+          type='button'
+          onClick={handleSearchInput}
+        >
+          <img src="/images/Magnifier.svg" alt="Lupa" />
+        </button>
+      </Link>
     </label>
   )
 }
