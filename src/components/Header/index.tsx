6ed6f8bfda641fbb7f96cdf96713829d07ec62bf
dvargas42 <dvargas.eng@gmail.com>
2021-05-22 23:48:07 -0300

    main page anchor creation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,15 @@
+import Link from 'next/link'
 import styles from './header.module.scss'
 import { SearchBox } from './SearchBox'
 
 export function Header() {
   return (
     <div className={styles.headerContainer}>
-      <img src="/images/Logotype.svg" alt="logo" />
+      <Link href={'/'}>
+        <a>
+          <img src="/images/Logotype.svg" alt="logo" />
+        </a>
+      </Link>
       <SearchBox />
     </div>
   )
