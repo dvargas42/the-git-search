0fa5b0f3067a71e16bb20e4289a0490260e1e41c
dvargas42 <dvargas.eng@gmail.com>
2021-05-23 23:06:11 -0300

    dynamic data implementation

--- src/components/UserCard/index.tsx
+++ src/components/UserCard/index.tsx
@@ -1,18 +1,47 @@
 import styles from './styles.module.scss'
 
-export function UserCard() {
+interface ItemData {
+  item: {
+    id: number;
+    login: string;
+    avatar_url: string;
+    url: string;
+    html_url: string;
+    score: number;
+    name: string;
+    created_at: string;
+  }
+  index: number
+}
+
+export function UserCard( {item, index} : ItemData) {
+
+  function lengthVerify(field: string) {
+    if (field.length > 25) {
+      return (field.slice(0, 23 - field.length ) + '...')
+    }
+    return field
+  }
+
   return (
-    <li className={styles.cardContainer}> 
-      <img src="/images/Avatar.png" alt="avatar" />
+    <li key={index} className={styles.cardContainer}> 
+      <img src={item.avatar_url} alt="avatar" />
 
       <div className={styles.cardContent}>
-        <h1>Milena Melo</h1>
+        <h1>{ item.name
+          ? lengthVerify(item.name)
+          : item.login
+        }
+        </h1>
 
-        <a href="">https://github.com/MilenaMelo</a>
+        <a href={item.html_url}>{item.html_url}</a>
 
-        <span>Score: 1.00</span>
+        <span>Score: {item.score}</span>
 
-        <button>
+        <button
+          type="button"
+          onClick={() => true}
+        >
           VER MAIS
         </button>
       </div>
