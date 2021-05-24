1336b6f97e5bd576f9053ef2aa43a52f5e195865
dvargas42 <dvargas.eng@gmail.com>
2021-05-24 04:30:15 -0300

    renaming environment variable

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,6 +3,6 @@ import axios from 'axios'
 export const api = axios.create({
   baseURL: 'https://api.github.com',
   headers: {
-    Authorization: `token ${process.env.GITHUB_TOKEN}`
+    Authorization: `token ${process.env.NEXT_PUBLIC_GITHUB_TOKEN}`
   }
 })
