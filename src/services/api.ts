7998189f11dc3f01cf8707216b46d6dcb8b23422
dvargas42 <dvargas.eng@gmail.com>
2021-05-23 22:56:58 -0300

    authorization header inclusion

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,8 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'https://api.github.com'
+  baseURL: 'https://api.github.com',
+  headers: {
+    Authorization: `token ${process.env.GITHUB_TOKEN}`
+  }
 })
