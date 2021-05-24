1e901a24febdb0b909fc807beb63642e3ed74ebb
dvargas42 <dvargas.eng@gmail.com>
2021-05-24 04:25:40 -0300

    data loading refactoring

--- src/pages/results/[search].tsx
+++ src/pages/results/[search].tsx
@@ -7,6 +7,8 @@ import { DateFormat, NumberFormat } from '../../utils/format'
 import { api } from '../../services/api'
 
 import styles from './results.module.scss'
+import { useEffect, useState } from "react";
+import { useRouter } from "next/dist/client/router";
 
 interface userProps {
   id: number;
@@ -23,10 +25,78 @@ interface SearchProps extends userProps {
 }
 
 interface SearchDataProps {
-  searchData: SearchProps;
+  data: SearchProps[];
+  total_count: number;
 }
 
-export default function Results({ searchData }: SearchDataProps) {
+interface DataProps {
+  data: SearchProps[];
+  total_count: number;
+  search: string;
+}
+
+async function SearchData (
+  search: string | string[],
+  page: number
+): Promise<SearchDataProps> {
+
+  const { data }= await api.get(`search/users?q=${search}`,{
+    params: {
+      per_page: 12,
+      page,
+    }
+  })
+
+  const searchData = await Promise.all(data.items.map(
+    async (user: userProps) => {
+      const { data } = await api.get(`users/${user.login}`)
+
+      return{
+        id: user.id,
+        name: data.name,
+        login: user.login,
+        avatar_url: user.avatar_url,
+        url: user.url, //
+        html_url: user.html_url,
+        score: NumberFormat(user.score),
+        created_at: DateFormat(data.created_at)
+      }
+    }
+  ))
+
+  return {
+    data: searchData,
+    total_count: data.total_count
+  }
+}
+
+export default function Results({ data, total_count, search }: DataProps) {
+  const [searchData, setSearchData] = useState<SearchProps[]>()
+  const [currentPage, setCurrentPage] = useState(2)
+  const [hasMorePages, setHasMorePages] = useState(true)
+  
+  const totalPages = Math.ceil(total_count / 12)
+  
+  useEffect(()=> {
+    setSearchData(data)    
+  }, [data])
+
+  async function NextPage() {
+    try {
+      if (currentPage > totalPages) {
+        setHasMorePages(!hasMorePages)
+        throw('No more pages')
+      }
+
+      const response = await SearchData(search, currentPage)
+      
+      setSearchData([...searchData, ...response.data])
+      setCurrentPage(currentPage + 1)
+    } catch (err) {
+      alert(err)
+    }
+  }
+
   return (
     <>
       <main className={styles.resultsContainer}>
@@ -45,10 +115,17 @@ export default function Results({ searchData }: SearchDataProps) {
                   index={ idx }
                 />
               )}
+              renderWhenEmpty={() => <div>Items não encontrados</div>}
+              hasMoreItens={true}
+              loadMoreItems={NextPage}
+              paginationLoadingIndicator={<div style={{background: '#090'}}>Getting more items...</div>}
+              //paginationLoadingIndicatorPosition="right"
             />
           </ul>
-
-          <div className={styles.space} />
+          {/* <button
+          onClick={NextPage}>
+            carregar
+          </button> */}
         </div>
       </main>
     </>
@@ -61,33 +138,16 @@ export const getStaticPaths: GetStaticPaths = async () => {
 }}
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
-  const { search } = ctx.params;
+  const { search } = ctx.params
+  const page = 1
 
-  const { data  } = await api.get(`search/users?q=${search}`)
-
-  const searchData = await Promise.all(data.items.map(
-
-    async (user: userProps) => {
-      const { data } = await api.get(`users/${user.login}`)
-
-      return{
-        id: user.id,
-        name: data.name,
-        login: user.login,
-        avatar_url: user.avatar_url,
-        url: user.url, //
-        html_url: user.html_url,
-        score: NumberFormat(user.score),
-        created_at: DateFormat(data.created_at)
-      }
-    }
-  ))
-
-  console.log(searchData)
+  const { data, total_count } = await SearchData(search, page)
 
   return {
     props: {
-      searchData: searchData
+      data,
+      total_count,
+      search,
     },
     revalidate: 60 * 60 * 24 // 24 hours
   }
