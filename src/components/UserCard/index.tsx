a7f74092ba50c85405e8b8a8e149676295fb4a2f
dvargas42 <dvargas.eng@gmail.com>
2021-05-24 22:59:44 -0300

    handleUserDetails function creation

--- src/components/UserCard/index.tsx
+++ src/components/UserCard/index.tsx
@@ -1,30 +1,44 @@
+import Link from 'next/link'
+import { useSearchData } from '../../hooks/useSearchData'
+
 import styles from './styles.module.scss'
 
-interface ItemData {
-  item: {
-    id: number;
-    login: string;
-    avatar_url: string;
-    url: string;
-    html_url: string;
-    score: number;
-    name: string;
-    created_at: string;
-  }
-  key: number
+interface UserProps {
+  id: number;
+  login: string;
+  avatar_url: string;
+  url: string;
+  html_url: string;
+  score: number;
+  name: string;
+  created_at: string;
+  followers: number,
+  following: number,
 }
 
-export function UserCard({ item } : ItemData) {
+interface ItemData {
+  item: UserProps;
+  key: number;
+}
+
+export function UserCard({ item, key } : ItemData) {
+  const { toggleUserModal, handleUser } = useSearchData()
 
   function lengthVerify(field: string) {
     if (field.length > 25) {
       return (field.substr(0, 22 ) + '...')
     }
+
     return field
   }
 
+  function handleUserDetails(user: UserProps) {
+    handleUser(user)
+    toggleUserModal()
+  }
+
   return (
-    <li className={styles.cardContainer}> 
+    <li key={key} className={styles.cardContainer}> 
       <img src={item.avatar_url} alt="avatar" />
 
       <div className={styles.cardContent}>
@@ -37,10 +51,10 @@ export function UserCard({ item } : ItemData) {
         <a href={item.html_url}>{item.html_url}</a>
 
         <span>Score: {item.score}</span>
-
+      
         <button
-          type="button"
-          onClick={() => true}
+          type='button'
+          onClick={() => handleUserDetails(item)}
         >
           VER MAIS
         </button>
